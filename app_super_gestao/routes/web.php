73fbfac2b806039db90faf4eca98341f571fb1af
kleitonalbuquerque <kleiton2102@gmail.com>
2020-12-30 17:08:31 -0300

    Rotas com parâmetros

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -30,3 +30,16 @@ Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 // });
 
 Route::get('/contato', 'ContatoController@contato');
+
+// Parâmetros: nome, categoria, titulo, mensagem
+Route::get('/contato/{titulo}/{nome}/{categoria}/{mensagem}', function(string $titulo, string $nome, string $categoria, string $mensagem) {
+    echo '<div class="container">';
+    echo "<h1>$titulo</h1>";
+    echo '<br>';
+    echo "Autor: $nome";
+    echo '<br>';
+    echo "Categoria: $categoria";
+    echo '<br>';
+    echo '<textarea placeholder="Texto">' . $mensagem . '</textarea>';
+    echo '</div>';
+});
